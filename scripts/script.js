1ade71481931ed75f19e4722ade53e64bdd61333
alexander <st235@yandex.ru>
2017-10-07 16:33:38 +0300

    Implemented sends

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,29 @@ var typed = new Typed('#typed', {
     typeSpeed: 60
 });
 
+subjectInput = document.getElementById("subject");
+emailInput = document.getElementById("email");
+messageInput = document.getElementById("message");
+sendButton = document.getElementById("submitButton");
+
+sendButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    var http = new XMLHttpRequest();
+    var url = "https://cv-mailer.herokuapp.com/email/send";
+    var params = "subject=" + subjectInput.value + "&from=" + emailInput.value + "&message=" + messageInput.value;
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+        }
+    }
+
+    http.send(params);
+    console.log("sended", params);
+});
 
 // Add Google Maps
 function initMap() {
