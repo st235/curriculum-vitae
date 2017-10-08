906086dfe601a10cf7a2f53034b4a0f021146cea
alexander <st235@yandex.ru>
2017-10-08 19:35:04 +0300

    Implemented navigation

--- scripts/script.js
+++ scripts/script.js
@@ -27,142 +27,21 @@ sendButton.addEventListener("click", function(e) {
     console.log("sended", params);
 });
 
-// Add Google Maps
-function initMap() {
-    var customMapType = new google.maps.StyledMapType(
-        [
-            {
-                "featureType": "road",
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "administrative",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#000000"
-                    },
-                    {
-                        "weight": 1
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "color": "#000000"
-                    },
-                    {
-                        "weight": 0.8
-                    }
-                ]
-            },
-            {
-                "featureType": "landscape",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#000000"
-                    }
-                ]
-            },
-            {
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            }
-        ], {
-      name: 'Styled'
-    });
-    var customMapTypeId = 'custom_style';
-  
-    var latLng = {
-      lat: 55.7547585,
-      lng: 37.6038036
-    };
-  
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 11,
-      center: latLng,
-      mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-      }
-    });
-  
-    map.mapTypes.set(customMapTypeId, customMapType);
-    map.setMapTypeId(customMapTypeId);
-  
-  }
+console.log($("#me-nav"));
+$("#me-nav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#me").offset().top},
+        'slow');
+});
+
+$("#projects-nav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#projects").offset().top},
+        'slow');
+});
+
+$("#contact-nav").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#footer").offset().top},
+        'slow');
+});
